60c5f7331f747510fb6cd96b02e15bbc435fbedf
Aly Mamdouh <aly.momdouh@gmail.com>
2023-04-23 15:42:50 +0200

    add select component and spinner in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { HeaderComponent } from './components/header/header.component';
+import { SelectComponent } from './components/select/select.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    SelectComponent,
+    SpinnerComponent
   ],
   imports: [
     CommonModule,
     SharedRoutingModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    SelectComponent,
+    SpinnerComponent
   ]
 })
 export class SharedModule { }
